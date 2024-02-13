a876a8e18c916b05426715ccab42c9d23862b224
nayoseb <nayoseb1018@gmail.com>
2024-02-13 13:17:57 +0900

    first commit

---
+++ src/main/java/com/example/jenkins/controller/TestController.java
@@ -0,0 +1,13 @@
+package com.example.jenkins.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    @GetMapping("/test")
+    public String test()
+    {
+        return "안녕하세요";
+    }
+}
